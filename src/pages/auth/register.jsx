669f0e3632057397a68f348781ac374b7103132c
Md Repon <repon7253@gmail.com>
2024-09-15 21:44:09 +0600

    if user email is already exists than registetion faild

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -28,6 +28,11 @@ const AuthRegister = () => {
           title : data?.payload?.message,
         })
         navigate('/auth/login')
+      } else {
+        toast({
+          title : data?.payload?.message,
+          variant: "destructive",
+        })
       }
     })
   }
