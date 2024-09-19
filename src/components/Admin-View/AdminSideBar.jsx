255722cdbac8fe1b6550ac1efd3c97626bf44434
Md Repon <repon7253@gmail.com>
2024-09-19 07:53:09 +0600

    side bar style and some minor component style

--- src/components/Admin-View/AdminSideBar.jsx
+++ src/components/Admin-View/AdminSideBar.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import { BadgeCheck, ChartNoAxesCombined, LayoutDashboard, ShoppingBasket } from 'lucide-react'
+import React, { Fragment } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard/>
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket/>
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BadgeCheck/>
+  }
+];
+
+
+function MenuItem(){
+  const navigate = useNavigate()
+  return <nav className='mt-8 flex flex-col gap-2'>
+    {
+      adminSidebarMenuItems.map((menuItem)=><div key={menuItem.id} onClick={()=>navigate(menuItem.path)} className='flex cursor-pointer text-2xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground'>
+        {menuItem.icon}
+        <span>{menuItem.label}</span>
+      </div>)
+    }
+  </nav>
+}
 
 const AdminSideBar = () => {
+
+  const navigate = useNavigate()
   return (
-    <div>
-      Admin Sidebar
-    </div>
+    <Fragment>
+      <aside className='hidden lg:flex w-64 flex-col border-r bg-background p-6'> 
+        <div onClick={()=>navigate("/admin/dashboard")} className='flex items-center gap-2 cursor-pointer'>
+          <ChartNoAxesCombined size={30}/>
+          <h1 className='text-xl font-extrabold'>Admin Pannel</h1>
+        </div>
+        <MenuItem/>
+      </aside>
+    </Fragment>
   )
 }
 
