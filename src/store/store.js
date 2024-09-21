4c3ca9494695047864d70ac0a4566f060dfb94de
Md Repon <repon7253@gmail.com>
2024-09-21 08:48:58 +0600

    create asyncThunk for add,fetchAllData,remove, edit(adminProductSlice>Store)

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
+import AdminProductSlice from "./admin/products-slice/AdminProductSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    AdminProductSlice: AdminProductSlice,
   },
 });
 
