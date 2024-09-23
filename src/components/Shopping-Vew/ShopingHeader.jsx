5b49e760da459f50ee3c726af4a854f40fe2c58e
Md Repon <repon7253@gmail.com>
2024-09-23 13:19:15 +0600

    header section design

--- src/components/Shopping-Vew/ShopingHeader.jsx
+++ src/components/Shopping-Vew/ShopingHeader.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import { HousePlug, Menu } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { useSelector } from "react-redux";
+import { ShoppingViewHeaderMenuIems } from "@/config";
 
-const ShopingHeader = () => {
+function MenuItems() {
   return (
-    <div>
-      Shoping header
-    </div>
-  )
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+      {ShoppingViewHeaderMenuIems.map((menuItem) => (
+        <Link
+          className="text-sm font-medium"
+          key={menuItem.id}
+          to={menuItem.path}
+        >
+          {menuItem.label}
+        </Link>
+      ))}
+    </nav>
+  );
 }
 
-export default ShopingHeader
+const ShopingHeader = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2 ">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="w-6 h-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+        {isAuthenticated ? <div></div> : null}
+      </div>
+    </header>
+  );
+};
+
+export default ShopingHeader;
--- src/config/index.js
+++ src/config/index.js
@@ -100,3 +100,42 @@ export const addProductFormElements = [
     placeholder: "Enter total stock",
   },
 ];
+
+export const ShoppingViewHeaderMenuIems = [
+  {
+    id: "home",
+    label: "home",
+    path: "/shop/home",
+  },
+
+  {
+    id: "men",
+    label: "Men",
+    path: "/shop/listing",
+  },
+  {
+    id: "women",
+    label: "Women",
+    path: "/shop/listing",
+  },
+  {
+    id: "kids",
+    label: "Kids",
+    path: "/shop/listing",
+  },
+  {
+    id: "watch",
+    label: "Watch",
+    path: "/shop/listing",
+  },
+  {
+    id: "footwear",
+    label: "Footwear",
+    path: "/shop/listing",
+  },
+  {
+    id: "accessories",
+    label: "Accessories",
+    path: "/shop/listing",
+  },
+];
