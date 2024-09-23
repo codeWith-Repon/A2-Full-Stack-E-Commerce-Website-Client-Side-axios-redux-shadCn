3a5db79781114a87bfe62ae0c1f37ec037f99bad
Md Repon <repon7253@gmail.com>
2024-09-23 20:35:36 +0600

    install checkbox and separetor.... show all filter optionn (shopingListing> ProductFilter> index)

--- src/pages/Shopping-view/ShopingListing.jsx
+++ src/pages/Shopping-view/ShopingListing.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import ProductFilter from "@/components/Shopping-Vew/ProductFilter";
+import React from "react";
 
 const ShopingListing = () => {
   return (
-    <div>
-     listing pagee 
+    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+      <ProductFilter />
     </div>
-  )
-}
+  );
+};
 
-export default ShopingListing
+export default ShopingListing;
