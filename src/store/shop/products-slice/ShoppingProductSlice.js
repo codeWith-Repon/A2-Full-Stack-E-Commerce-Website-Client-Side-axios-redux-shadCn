765a8b142d478f8811bfe5e20b8615add3889813
Md Repon <repon7253@gmail.com>
2024-09-25 09:13:06 +0600

    (D2nd) create slice and call api and show data in components (shoppingProductSlice > Store > ShoppingListing > shoppingProducTile)

---
+++ src/store/shop/products-slice/ShoppingProductSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//2nd
+const initialState = {
+  isLoading: false,
+  productList: [],
+};
+
+//3rd
+export const fetchAllFilterdProducts = createAsyncThunk(
+  "/products/fetchAllFilterdProducts",
+  async () => {
+    const result = await axios.get(
+      "http://localhost:5000/api/admin/products/get"
+    );
+    return result?.data;
+  }
+);
+
+//1st
+const shoppingProductsSlice = createSlice({
+  name: "shoppingProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    //4th
+    builder
+      .addCase(fetchAllFilterdProducts.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllFilterdProducts.fulfilled, (state, action) => {
+        console.log("action.payload", action.payload);
+
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllFilterdProducts.rejected, (state, action) => {
+        (state.isLoading = false), (state.productList = []);
+      });
+  },
+});
+
+export default shoppingProductsSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import AdminProductSlice from "./admin/products-slice/AdminProductSlice";
+import shopProductsSlice from "./shop/products-slice/ShoppingProductSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     AdminProducts: AdminProductSlice,
+    shopProducts: shopProductsSlice,
   },
 });
 
