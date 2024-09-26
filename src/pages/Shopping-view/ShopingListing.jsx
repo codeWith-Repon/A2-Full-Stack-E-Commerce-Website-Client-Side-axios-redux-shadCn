ac9b6a62e50dfb36eb3d545b8e449255cde45c47
Md Repon <repon7253@gmail.com>
2024-09-26 08:32:20 +0600

    when checked box is checked then send the checked value send into url

--- src/pages/Shopping-view/ShopingListing.jsx
+++ src/pages/Shopping-view/ShopingListing.jsx
@@ -13,12 +13,27 @@ import { fetchAllFilterdProducts } from "@/store/shop/products-slice/ShoppingPro
 import { ArrowUpDown } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+
+function createSearchParamsHelper(filterParams){
+  const queryParams = [];
+
+  for(const [key,value] of Object.entries(filterParams)){
+    if(Array.isArray(value) && value.length > 0){
+      const paramValue = value.join(",")
+
+      queryParams.push(`${key}=${encodeURIComponent(paramValue)}`)
+    }
+  }
+  return queryParams.join('&')
+}
 
 const ShopingListing = () => {
   const dispatch = useDispatch();
   const { productList } = useSelector((state) => state.shopProducts);
   const [filters, setFilters] = useState({})
   const [sort, setSort] = useState(null)
+  const [searchParams, setSearchParams] = useSearchParams()
 
   function handleSort(value){
     // console.log(value)
@@ -52,15 +67,21 @@ const ShopingListing = () => {
     setFilters(JSON.parse(sessionStorage.getItem('filters'))|| {})
   },[])
 
+  useEffect(()=>{
+    if(filters && Object.keys(filters).length > 0){
+      const createQuireyString = createSearchParamsHelper(filters)
+      setSearchParams(new URLSearchParams(createQuireyString))
+    }
+  },[filters])
 
   useEffect(() => {
     dispatch(fetchAllFilterdProducts());
   }, [dispatch]);
 
-  console.log("copyfilters:", filters);
+  console.log("searchParams:", searchParams.toString());
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
       <ProductFilter filters={filters} handleFilter={handleFilter}/>
       <div className="bg-background w-full rounded-lg shadow-sm">
         <div className="p-4 border-b flex items-center justify-between">
