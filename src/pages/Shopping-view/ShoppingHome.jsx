05b656bdbe4d16404593589a67c8011534634304
Md Repon <repon7253@gmail.com>
2024-10-02 11:37:44 +0600

    slide autometic change and slide button is functional

--- src/pages/Shopping-view/ShoppingHome.jsx
+++ src/pages/Shopping-view/ShoppingHome.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import bannerOne from "../../assets/banner-1.webp";
 import bannerTwo from "../../assets/banner-2.webp";
@@ -14,16 +14,25 @@ import {
 } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 
+const categoriesWithIcon = [
+  { id: "men", label: "Men", icon: ShirtIcon },
+  { id: "women", label: "Women", icon: CloudLightning },
+  { id: "kids", label: "Kids", icon: BabyIcon },
+  { id: "accessories", label: "Accessories", icon: WatchIcon },
+  { id: "footwear", label: "Footwear", icon: UmbrellaIcon },
+];
+
 const ShoppingHome = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const slides = [bannerOne, bannerTwo, bannerThree];
 
-  const categoriesWithIcon = [
-    { id: "men", label: "Men", icon: ShirtIcon },
-    { id: "women", label: "Women", icon: CloudLightning },
-    { id: "kids", label: "Kids", icon: BabyIcon },
-    { id: "accessories", label: "Accessories", icon: WatchIcon },
-    { id: "footwear", label: "Footwear", icon: UmbrellaIcon },
-  ];
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 5000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -32,12 +41,19 @@ const ShoppingHome = () => {
           <img
             src={slide}
             key={index}
-            className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000`}
+            className={`${
+              index === currentSlide ? "opacity-100" : "opacity-0"
+            } absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000`}
           />
         ))}
         <Button
           variant="outline"
           size="icon"
+          onClick={() =>
+            setCurrentSlide(
+              (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+            )
+          }
           className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white/80"
         >
           <ChevronLeftIcon className="w-4 h-4" />
@@ -45,6 +61,9 @@ const ShoppingHome = () => {
         <Button
           variant="outline"
           size="icon"
+          onClick={() =>
+            setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length)
+          }
           className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/80"
         >
           <ChevronRightIcon className="w-4 h-4" />
