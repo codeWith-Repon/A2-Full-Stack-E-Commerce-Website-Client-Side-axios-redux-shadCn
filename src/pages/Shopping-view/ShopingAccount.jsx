a46600baf67e61cf70ee3a7cab0da4a7a0a2ecbf
Md Repon <repon7253@gmail.com>
2024-10-03 21:29:12 +0600

    install tabs and settin up tabs content

--- src/pages/Shopping-view/ShopingAccount.jsx
+++ src/pages/Shopping-view/ShopingAccount.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import accImg from "../../assets/account.jpg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Order from "@/components/Shopping-Vew/Order";
+import Address from "@/components/Shopping-Vew/Address";
 
 const ShopingAccount = () => {
   return (
-    <div>
-      shop account page
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full overflow-hidden">
+        <img
+          src={accImg}
+          alt="banner Image"
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
+        <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
+          <Tabs defaultValue="orders">
+            <TabsList>
+              <TabsTrigger value="orders">Orders</TabsTrigger>
+              <TabsTrigger value="address">Address</TabsTrigger>
+            </TabsList>
+            <TabsContent value="orders">
+              <Order />
+            </TabsContent>
+            <TabsContent value="address">
+              <Address />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShopingAccount
+export default ShopingAccount;
