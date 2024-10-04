bb6f2f8bcbf23885b2dcb90c8cb966458897d336
Md Repon <repon7253@gmail.com>
2024-10-04 10:45:23 +0600

    create form index > address > addressCard

--- src/components/Shopping-Vew/Address.jsx
+++ src/components/Shopping-Vew/Address.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import CommonForm from "../common/CommonForm";
+import { addressFormControls } from "@/config";
+
+const initialAddressFormData = {
+  address: "",
+  city: "",
+  phone: "",
+  pincode: "",
+  notes: "",
+};
 
 const Address = () => {
-  return (
-    <div>
-      Address
-    </div>
-  )
-}
+  const [formData, setFormData] = useState(initialAddressFormData);
 
-export default Address
+  function handleManageAddress(e) {
+    e.preventDefault();
+  }
+
+  function isFormValid() {
+    return Object.keys(formData)
+      .map((key) => formData[key].trim() !== "")
+      .every((item) => item);
+  }
+
+  return (
+    <Card>
+      <div className="pl-6 pt-2">Address List</div>
+      <CardHeader>
+        <CardTitle>Add New Address</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-3">
+        <CommonForm
+          formControls={addressFormControls}
+          formData={formData}
+          setFormData={setFormData}
+          buttonText={"Add"}
+          onSubmit={handleManageAddress}
+          isButtonDisabled={!isFormValid()}
+        />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Address;
