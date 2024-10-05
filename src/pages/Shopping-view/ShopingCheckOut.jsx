9b6769364d30b903ec5ae1eab66d02fd07e79aa4
Md Repon <repon7253@gmail.com>
2024-10-05 07:02:06 +0600

    render cart total

--- src/pages/Shopping-view/ShopingCheckOut.jsx
+++ src/pages/Shopping-view/ShopingCheckOut.jsx
@@ -2,10 +2,24 @@ import Address from "@/components/Shopping-Vew/Address";
 import img from "../../assets/account.jpg";
 import UserCartItemsContent from "@/components/Shopping-Vew/UserCartItemsContent";
 import { useSelector } from "react-redux";
+import { Button } from "@/components/ui/button";
 
 const ShopingCheckOut = () => {
   const { cartItems } = useSelector((state) => state.shopCart);
   console.log("reponX", cartItems);
+
+  const totalCartAmount =
+    cartItems && cartItems.items && cartItems.items.length > 0
+      ? cartItems.items.reduce(
+          (sum, currentItem) =>
+            sum +
+            (currentItem?.salePrice > 0
+              ? currentItem?.salePrice
+              : currentItem?.price) *
+              currentItem?.quantity,
+          0
+        )
+      : 0;
 
   return (
     <div className="flex flex-col">
@@ -24,6 +38,15 @@ const ShopingCheckOut = () => {
                 <UserCartItemsContent cartItem={item} />
               ))
             : null}
+          <div className="mt-8 space-y-4">
+            <div className="flex justify-between">
+              <span className="font-bold">Total</span>
+              <span className="font-bold">${totalCartAmount}</span>
+            </div>
+          </div>
+          <div className="mt-4 w-full">
+            <Button className="w-full">Checkout with Paypal</Button>
+          </div>
         </div>
       </div>
     </div>
