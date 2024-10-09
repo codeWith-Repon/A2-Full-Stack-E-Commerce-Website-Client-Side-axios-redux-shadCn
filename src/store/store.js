87e8d47a0875fead77a103996048d65bafe0ea9b
Md Repon <repon7253@gmail.com>
2024-10-09 22:19:38 +0600

    create slice OrderSlice > Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import AdminProductSlice from "./admin/products-slice/AdminProductSlice";
+import adminOrderSlice from "./admin/order-slice/OrderSlice";
+
 import shopProductsSlice from "./shop/products-slice/ShoppingProductSlice";
 import shopCartSlice from "./shop/Cart-Slice/ShopCartSlice";
 import shopAddressSlice from "./shop/address-slice/AddressSlice";
@@ -9,7 +11,10 @@ import shopOrderSlice from "./shop/Order-Slice/OrderSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
+
     AdminProducts: AdminProductSlice,
+    adminOrder: adminOrderSlice,
+    
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
     shopAddress: shopAddressSlice,
