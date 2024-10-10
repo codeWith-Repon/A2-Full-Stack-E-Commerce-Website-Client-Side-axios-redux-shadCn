12238a51eab243080703e9d17d1f331752121997
Md Repon <repon7253@gmail.com>
2024-10-10 23:06:37 +0600

    R2 check out of product condition work or not

--- src/pages/Shopping-view/ShopingListing.jsx
+++ src/pages/Shopping-view/ShopingListing.jsx
@@ -116,6 +116,7 @@ const ShopingListing = () => {
     
   },[productDetails])
 
+  console.log(productList,"productList123")
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
