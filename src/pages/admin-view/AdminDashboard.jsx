bcefa33c9040a3c97d49088d5d1b4ad2d8069ca4
Md Repon <repon7253@gmail.com>
2024-10-14 19:07:34 +0600

    upload and render iamge

--- src/pages/admin-view/AdminDashboard.jsx
+++ src/pages/admin-view/AdminDashboard.jsx
@@ -1,6 +1,9 @@
 import ProductImageUpload from "@/components/Admin-View/ProductImageUpload";
 import { Button } from "@/components/ui/button";
-import { addFeatureImages, getFeatureImages } from "@/store/CommonSlice/commonSlice";
+import {
+  addFeatureImages,
+  getFeatureImages,
+} from "@/store/CommonSlice/commonSlice";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -9,24 +12,26 @@ const AdminDashboard = () => {
   const [uploadedImageUrl, setUploadedImageUrl] = useState("");
   const [imageLoadingState, setImageLoadingState] = useState(false);
   const dispatch = useDispatch();
-  const {featureImageList} = useSelector(state => state.commonFeature)
+  const { featureImageList } = useSelector((state) => state.commonFeature);
 
-  console.log(uploadedImageUrl,"uploadedImageUrl")
+  console.log(uploadedImageUrl, "uploadedImageUrl");
 
-  function handleUploadFeatureImage(){
-    dispatch(addFeatureImages(uploadedImageUrl)).then((data)=>{
-      console.log("datax", data)
-    })
+  function handleUploadFeatureImage() {
+    dispatch(addFeatureImages(uploadedImageUrl)).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(getFeatureImages());
+      }
+    });
   }
 
-  useEffect(()=>{
-    dispatch(getFeatureImages())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(getFeatureImages());
+  }, [dispatch]);
 
-  console.log(featureImageList,"featureimage")
+  console.log(featureImageList, "featureimage");
 
   return (
-    <div>
+    <div className="flex flex-col gap-4 mt-5">
       <ProductImageUpload
         imageFile={imageFile}
         setImageFile={setImageFile}
@@ -37,7 +42,20 @@ const AdminDashboard = () => {
         isCustomStyle={true}
         // currentEditedId={currentEditedId !== null}
       />
-      <Button onClick={handleUploadFeatureImage} className="mt-5 w-full">Upload</Button>
+      <Button onClick={handleUploadFeatureImage} className="mt-5 w-full">
+        Upload
+      </Button>
+      <div className="flex flex-col gap-4 mt-5">
+        {featureImageList && featureImageList.length > 0
+          ? featureImageList.map((featureImageItem) => (
+              <img
+                src={featureImageItem.image}
+                alt=""
+                className="w-full h-[300px] object-cover rounded-t-lg"
+              />
+            ))
+          : null}
+      </div>
     </div>
   );
 };
